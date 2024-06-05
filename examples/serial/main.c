1ff84748df0fed8aeb40b2da8e3a5d3209247caa
Sandeep Mistry <sandeep.mistry@arm.com>
2024-06-05 15:41:01 -0400

    Add serial example

---
+++ examples/serial/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <pico/stdlib.h>
+
+#include "rnnoise.h"
+
+int main()
+{
+  stdio_init_all();
+
+  while (!stdio_usb_connected()) {
+    tight_loop_contents();
+  }
+
+  #define FRAME_SIZE 480
+  int16_t x_i16[FRAME_SIZE];
+  float x_f32[FRAME_SIZE];
+  uint8_t* x_i8 = (uint8_t*)x_i16;
+
+  DenoiseState* st = rnnoise_create(NULL);
+
+  // run once to init
+  memset(x_f32, 0x00, sizeof(x_f32));
+  rnnoise_process_frame(st, x_f32, x_f32);
+
+  while (1) {
+    for (size_t i = 0; i < sizeof(x_i16); i++) {
+      x_i8[i] = getchar();
+    }
+
+    for (int i = 0; i < FRAME_SIZE; i++) {
+      x_f32[i] = x_i16[i];
+    }
+
+    float vad = rnnoise_process_frame(st, x_f32, x_f32);
+
+    for (int i = 0; i < FRAME_SIZE; i++) {
+      x_i16[i] = x_f32[i];
+    }
+
+    for (int i = 0; i < sizeof(x_i16); i++) {
+      putchar_raw(x_i8[i]);
+    }
+  }
+
+  return 0;
+}
---
+++ examples/serial/serial_denoise.py
@@ -0,0 +1,23 @@
+import sys
+
+import serial
+
+port = sys.argv[1]
+input_file = sys.argv[2]
+output_file = sys.argv[3]
+
+with serial.Serial(port, 115200, timeout=1) as ser:
+  with open(input_file, 'rb') as input:
+    with open(output_file, 'wb') as output:
+      while True:
+        in_ = input.read(480 * 2)
+        if len(in_) == 0:
+          break
+
+        ser.write(in_)
+        out = ser.read(480 * 2)
+        output.write(out)
+
+        print('.', end='')
+
+print(' done!')
